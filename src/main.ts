d0536e8fe4ffdf6b91a421b113b176bf0ebc97c5
Ivan Koliada <ivanKoliada69@gmail.com>
2023-01-31 23:24:08 +0300

    feat: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const port = process.env.PORT || 4000;
 
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+
+  const file = readFileSync('./doc/api.yaml', 'utf8');
+  const document = parse(file);
+
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(port);
 }
