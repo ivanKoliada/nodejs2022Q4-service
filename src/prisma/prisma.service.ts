b2cc1540d8e74d233f462af4f5b9f4179c9a471b
Ivan Koliada <ivanKoliada69@gmail.com>
2023-02-05 17:07:07 +0300

    feat: test docker compose

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,5 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { AlbumEntity } from 'src/albums/albums.entity';
-import { ArtistEntity } from 'src/artists/artists.entity';
-import { TrackEntity } from 'src/tracks/tracks.entity';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
