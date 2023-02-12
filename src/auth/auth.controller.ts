ca41e9dccbf709d404c27c500d4778fd2049b755
Ivan Koliada <ivanKoliada69@gmail.com>
2023-02-12 11:59:07 +0300

    feat: sign up route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserEntity } from 'src/users/users.entity';
+import { AuthDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body() authDto: AuthDto) {
+    const user = (await this.authService.signUp(
+      authDto,
+    )) as unknown as UserEntity;
+
+    return new UserEntity(user);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { AuthDto } from './auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService, // private readonly userService: UsersService,
+  ) {}
+
+  async signUp(authDto: AuthDto) {
+    return await this.prisma.user.create({
+      data: authDto,
+    });
+  }
+}
