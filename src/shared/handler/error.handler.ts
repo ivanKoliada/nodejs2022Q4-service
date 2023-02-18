acf04d9b185cf12734ce61ba923b76398967177d
Ivan Koliada <ivanKoliada69@gmail.com>
2023-02-18 13:50:07 +0300

    feat: prod and dev docker mode

--- src/shared/handler/error.handler.ts
+++ src/shared/handler/error.handler.ts
@@ -3,11 +3,11 @@ import { LoggingService } from '../logger/logging.service';
 
 export const loggingHandledErrors = (logger: LoggingService) => {
   process.on('unhandledRejection', (error) =>
-    logger.error(MSG.UNHANDLED_REJECTION),
+    logger.error(`${MSG.UNHANDLED_REJECTION} ${error}`),
   );
 
   process.on('uncaughtException', (error) => {
-    logger.error(MSG.UNCAUGHT_EXCEPTION);
+    logger.error(`${MSG.UNCAUGHT_EXCEPTION} ${error}`);
     process.exit(1);
   });
 };
